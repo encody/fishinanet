7588e6719d1b54367356e24f4b6e80c209c16937
Jacob <encody@users.noreply.github.com>
2019-02-03 05:44:41 -0600

    adds working state

--- server/src/index.ts
+++ server/src/index.ts
@@ -9,21 +9,64 @@ import { TempReading } from "./entity/TempReading";
 import { User } from "./entity/User";
 import { DefaultRepositoryEndpointController } from "./RepositoryController";
 import * as cors from 'cors';
+import * as crypto from 'crypto';
+import * as cookieParser from 'cookie-parser';
 import { hashPasswordAndSalt } from "./hash";
+
+const sessionCookie = 'fishina-session';
 
 createConnection().then(async connection => {
     const app = express();
     app.use(bodyParser.json());
     app.use(cors());
+    app.use(cookieParser());
+
+    const sessions: Map<string, {
+        userId: number,
+        expires: number,
+    }> = new Map();
 
     const userRepo = connection.getRepository(User);
 
-    // app.post('/login', (req: express.Request, res: express.Response) => {
-    //     const user = userRepo.findOne({
-    //         userName: req.body.userName,
-    //         pass: hashPasswordAndSalt(req.body.pass),
-    //     })
-    // });
+    userRepo.create({
+
+    })
+
+    app.post('/login', async (req: express.Request, res: express.Response) => {
+        const user = await userRepo.findOne({
+            userName: req.body.userName,
+        });
+
+        if (user) {
+            const hashed = hashPasswordAndSalt(
+                req.body.pass,
+                user.salt,
+            );
+
+            if (Buffer.compare(user.pass, hashed) === 0) {
+                const key = crypto.randomBytes(32).toString('hex');
+                sessions.set(key, {
+                    userId: user.id,
+                    // Expire session in 3 hours
+                    expires: Date.now() + 1000 * 60 * 60 * 3,
+                });
+                res.cookie(sessionCookie, key).send(true);
+                return;
+            }
+        }
+
+        res.clearCookie(sessionCookie).send(false);
+    });
+
+    app.all('/logout', async (req: express.Request, res: express.Response) => {
+        res.clearCookie(sessionCookie).send(true);
+    });
+
+    // Verify login session
+    app.use((req: express.Request, res: express.Response, next: express.NextFunction) => {
+        console.log(req.cookies);
+        next();
+    });
 
     const entities: {schema: any, path: string}[] = [
         { schema: User,          path: '/user',          },
