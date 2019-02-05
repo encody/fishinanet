ec6ca518fbb046661b67a5dc63a81859faa55369
Jacob <encody@users.noreply.github.com>
2019-02-05 05:06:14 +0000

    moves server to different repo

--- server/src/hash.ts
+++
@@ -1,9 +0,0 @@
-import * as crypto from 'crypto';
-
-export function hashPasswordAndSalt (pass: string, salt: Buffer): Buffer {
-    return crypto.createHash('sha256')
-        .update(Buffer.from(pass))
-        .update(salt)
-        .digest()
-    ;
-}
